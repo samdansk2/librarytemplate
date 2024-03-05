f8d20123235d57a2ff1861b980ca7d1c35d5a213
samdansk2 <samdanshaik8998@gmail.com>
2024-03-05 09:58:40 +0530

    yml

--- src/librarytemplate/read_yml.py
+++ src/librarytemplate/read_yml.py
@@ -1,1 +1,18 @@
-#TODO
+import yaml
+
+# path to YAML file
+yaml_file_path = "src\librarytemplate\data\input,yml"
+
+# Read the YAML file
+with open(yaml_file_path, "r") as yaml_file:
+    yaml_data = yaml.safe_load(yaml_file)
+
+# Access and work with the YAML data
+temperature = yaml_data["temperature"]
+settings = yaml_data["settings"]
+data = yaml_data["data"]
+
+# Print some example data to verify it's loaded correctly
+print("Temperature:", temperature)
+print("Settings X:", settings["x"])
+print("Settings Y:", settings["y"])
