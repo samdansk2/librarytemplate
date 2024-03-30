621677fe45be7a1f7aed1f43023ec02ef439a3ea
Samdan Shaik <110446718+samdansk2@users.noreply.github.com>
2024-03-30 10:10:01 +0530

    Deployment (#7)
    * update_tomlfile
    
    * distribution_files

---
+++ src/librarytemplate/__main__.py
@@ -0,0 +1,24 @@
+"""Generic python package
+
+Usage:
+------
+
+
+
+Contact:
+--------
+
+More information is available at:
+
+- https://pypi.org/project/librarytemplate/
+- https://github.com/samdansk2/librarytemplate
+
+
+Version:
+--------
+
+- librarytemplate v0.0.1
+"""
+from librarytemplate.engine import engine
+
+engine()
